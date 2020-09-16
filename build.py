3100451d258aa635f8832c558b0156b519095c18
Chris Marsh <chrismarsh.c2@gmail.com>
2020-09-16 17:56:15 -0600

    fix trilinos:shared

--- build.py
+++ build.py
@@ -6,15 +6,19 @@ if __name__ == "__main__":
                                 archs=["x86_64"],
                                 build_types=["Release"])
                               
-    builder.add_common_builds(pure_c=False,shared_option_name=False)
+    builder.add_common_builds(pure_c=False,shared_option_name='trilinos:shared')
 
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
 
     named_builds = defaultdict(list)
     for settings, options, env_vars, build_requires, reference in builder.items:
 
+        shared="shared"
+
+        if not options['trilinos:shared']:
+            shared = "static" 
       
-        named_builds[settings['compiler']+"_static" ].append([settings, options, env_vars, build_requires, reference])
+        named_builds[settings['compiler']+shared ].append([settings, options, env_vars, build_requires, reference])
 
     builder.named_builds = named_builds
 
